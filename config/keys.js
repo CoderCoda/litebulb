6e2b1f36cceb5177d97f6ccfa4e25064248243af
CoderCoda <mitchell.li.duke@gmail.com>
2018-08-27 22:48:25 -0400

    handled production keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,1 @@
-module.exports = {
-    mongoURI: (process.env.NODE_ENV == "production")?"mongodb://mitchell:mitchym1tch@ds249565.mlab.com:49565/litebulb-prod":"mongodb://localhost/litebulb-dev",
-    googleClientID: "939735003009-d0aq0pdrhqsb07ndrb83m0kqasfcijhm.apps.googleusercontent.com",
-    googleClientSecret: "_OkhwuTDMUeKfdL7tsjzhZVM"
-};
+module.exports = (process.env.NODE_ENV == "production")? require("./keys_prod"):require("./keys_dev");
